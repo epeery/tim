b5a0ff60250f1b0ac6f8c39d90162ed9b04ebde1
Eli Peery <eli.g.peery@gmail.com>
2019-04-27 11:10:47 -0400

    :sparkles: Added confirmation
    Added confirmation when removing a project

--- src/commands/remove/index.ts
+++ src/commands/remove/index.ts
@@ -1,5 +1,6 @@
 import {Command, flags} from '@oclif/command'
 import {existsSync, outputJson, readJSON, remove} from 'fs-extra'
+import {prompt} from 'inquirer'
 
 import {autocompleteProject} from '../../autocomplete-project'
 import {getCurrentProjectFile} from '../../get-current-project-file'
@@ -15,14 +16,14 @@ export default class Remove extends Command {
 
   static flags = {
     help: flags.help({char: 'h'}),
+    confirm: flags.boolean({char: 'c'}),
   }
 
   static args = [{name: 'project'}]
 
   async run() {
-    const {args} = this.parse(Remove)
+    const {args, flags} = this.parse(Remove)
     let project = args.project
-
     let projectFile = await getProjectDir(this.config.configDir, project)
 
     if (!project || !existsSync(projectFile)) {
@@ -33,15 +34,24 @@ export default class Remove extends Command {
       projectFile = await getProjectDir(this.config.configDir, project)
     }
 
-    const currentProjectFile = await getCurrentProjectFile(this.config.configDir)
-    const current = await readJSON(currentProjectFile)
-
-    remove(projectFile)
-
-    if (current.name === project) {
-      await outputJson(currentProjectFile, {})
+    let confirm = {confirm: false}
+    if (!flags.confirm) {
+      confirm = await prompt({type: 'confirm', name: 'confirm', message: `Are you sure you want to remove: ${project}?`})
     }
 
-    this.log(`The project "${project}" was removed`)
+    if (flags.confirm || confirm.confirm) {
+      const currentProjectFile = await getCurrentProjectFile(this.config.configDir)
+      const current = await readJSON(currentProjectFile)
+
+      remove(projectFile)
+
+      if (current.name === project) {
+        await outputJson(currentProjectFile, {})
+      }
+
+      this.log(`The project "${project}" was removed`)
+    } else {
+      this.log(`The project "${project}" was not removed`)
+    }
   }
 }
