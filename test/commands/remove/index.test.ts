cce41ee31f95b0f1d63cf28672e55e18e9668c68
Eli Peery <eli.g.peery@gmail.com>
2019-04-30 13:25:06 -0400

    :white_check_mark: Updated tests

--- test/commands/remove/index.test.ts
+++ test/commands/remove/index.test.ts
@@ -1,17 +1,22 @@
 import {expect, test} from '@oclif/test'
+import {remove} from 'fs-extra'
 
 describe('remove:index', () => {
   test
+    .env({XDG_CONFIG_HOME: './test/test-config'})
+    .command(['add', 'remove-test'])
     .stdout()
-    .command(['remove:index'])
-    .it('runs hello', ctx => {
-      expect(ctx.stdout).to.contain('hello world')
+    .command(['remove', 'remove-test', '-c'])
+    .do(() => remove('./test/test-config'))
+    .it('removes project', ctx => {
+      expect(ctx.stdout).to.contain('The project "remove-test" was removed')
     })
 
   test
+    .env({XDG_CONFIG_HOME: './test/test-config'})
     .stdout()
-    .command(['remove:index', '--name', 'jeff'])
-    .it('runs hello --name jeff', ctx => {
-      expect(ctx.stdout).to.contain('hello jeff')
-    })
+    .command(['remove', 'remove-test', '-c'])
+    .exit(2)
+    .do(() => remove('./test/test-config'))
+    .it('exits if no project exists')
 })
--- test/commands/remove/session.test.ts
+++ test/commands/remove/session.test.ts
@@ -1,17 +1,43 @@
 import {expect, test} from '@oclif/test'
+import {readdirSync, remove, writeJson} from 'fs-extra'
+
+let dayFile: string
+
+let newDayFile = {
+  date: '2019-04-30T17:03:13.408Z',
+  sessions: [
+    {
+      name: 'remove-test',
+      start: '2019-04-30T17:03:11.101Z',
+      end: '2019-04-30T17:03:13.408Z',
+      time: 2307,
+      notes: [],
+      id: 'q-yJFEggG'
+    }
+  ]
+}
 
 describe('remove:session', () => {
   test
+    .env({XDG_CONFIG_HOME: './test/test-config'})
+    .command(['add', 'remove-test'])
+    .command(['start', 'remove-test'])
+    .command(['stop'])
+    .do(() => dayFile = readdirSync('./test/test-config/tim-time/sessions')[0])
+    .do(() => writeJson(`./test/test-config/tim-time/sessions/${dayFile}`, newDayFile))
     .stdout()
-    .command(['remove:session'])
-    .it('runs hello', ctx => {
-      expect(ctx.stdout).to.contain('hello world')
+    .command(['remove:session', 'q-yJFEggG', '-c'])
+    .do(() => remove('./test/test-config'))
+    .it('removes session', ctx => {
+      expect(ctx.stdout).to.contain('Removed session with ID: q-yJFEggG')
     })
 
   test
+    .env({XDG_CONFIG_HOME: './test/test-config'})
     .stdout()
-    .command(['remove:session', '--name', 'jeff'])
-    .it('runs hello --name jeff', ctx => {
-      expect(ctx.stdout).to.contain('hello jeff')
+    .command(['remove:session', 'blah'])
+    .do(() => remove('./test/test-config'))
+    .it('no sessions', ctx => {
+      expect(ctx.stdout).to.contain("You haven't logged any sessions today")
     })
 })
