a6699c05269049eba8d9384043e9efda9e14ac7b
Eli Peery <eli.g.peery@gmail.com>
2019-05-05 00:14:32 -0400

    :recycle: Refactored to a more functional style

--- src/commands/add.ts
+++ src/commands/add.ts
@@ -19,19 +19,45 @@ export default class Add extends Command {
   async run() {
     const {args} = this.parse(Add)
 
-    let project = args.project
+    const Right = (x: any) => ({
+      x,
+      map: (f: any) => Right(f(x)),
+      fold: (_: any, g: any) => g(x),
+      chain: (f: any) => f(x),
+      ap: (o: any) => x(o.x),
+      inspect: () => `Right(${x})`
+    })
 
-    if (!args.project) {
-      project = await cli.prompt('What is your project called?')
-    }
+    const Left = (x: any) => ({
+      x,
+      map: (_: any) => Left(x),
+      fold: (f: any, _: any) => f(x),
+      chain: (_: any) => Left(x),
+      ap: (_: any) => Left(x),
+      inspect: () => `Left(${x})`
+    })
+
+    // eitherFromNullable :: a -> Either a
+    const eitherFromNullable = (x: any) => (x !== null && x !== undefined) ? Right(x) : Left(null)
+
+    // eitherFromNullable :: Bool -> Either Bool
+    const eitherFromBool = (x: boolean) => x ? Right(x) : Left(false)
+
+    const project = await eitherFromNullable(args.project)
+      .fold(
+        () => cli.prompt('What is your project called?')
+        , (x: string) => x
+      )
 
     const projectFile = await getProjectFile(this.config.configDir, project)
 
-    if (existsSync(projectFile)) {
-      this.error('Project already exists')
-    } else {
-      await outputJson(projectFile, {name: project})
-      this.log(`The project: "${project}" was created`)
-    }
+    eitherFromBool(await existsSync(projectFile))
+      .fold(
+        async () => {
+          await outputJson(projectFile, {name: project})
+          this.log(`The project: "${project}" was created`)
+        }
+        , () => this.error('Project already exists')
+      )
   }
 }
